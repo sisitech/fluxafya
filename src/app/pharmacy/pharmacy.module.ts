b8d2067e317c7baf2df98d9d493f48fad07bfc30
Shimanyi Valentino <shimanyi@sisitech.com>
2019-01-11 18:05:58 +0300

    changed component route in pharmacy/

--- src/app/pharmacy/pharmacy.module.ts
+++ src/app/pharmacy/pharmacy.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PharmacyComponent } from './pharmacy/pharmacy.component';
-import { PatientsComponent } from '../doctors/patients/patients.component';
+import { PatientsComponent } from '../pharmacy/patients/patients.component';
 import { RouterModule } from '@angular/router';
 
 import { PharmacyRoutes } from './pharmacy.routing';
