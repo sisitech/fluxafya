309dbea1c948fc0e22d089e1b35e09b29db224cf
Dennis Chivatsi <dennischivatsi@gmail.com>
2019-01-19 14:57:58 +0300

    Labs Module

---
+++ src/app/labs/labs.routing.ts
@@ -0,0 +1,24 @@
+import { LabsComponent } from './labs.component';
+import { Routes } from '@angular/router';
+import { PatientsComponent } from './patients/patients.component';
+
+export const LabRoutes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'see-patient',
+        component: LabsComponent,
+        data: {
+          heading: "Lab's Form"
+        }
+      },{
+        path: '**',
+        component: PatientsComponent,
+        data: {
+          heading: 'Patients'
+        }
+      }
+    ]
+  }
+];
