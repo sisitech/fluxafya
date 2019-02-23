ed909cb8f95d76dad7a76343b0a39e25154b1680
Norman Munge <normanmunge@gmail.com>
2019-02-23 23:43:42 +0300

    Started working on login integrations, changed login logo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true
 };
+
+export const  BaseUrl = Object.freeze({
+  base_api_url: 'http://api.fluxafya.com/',
+});
+
+// set this to true to use static data from "data" folder
+export const staticData = true;
